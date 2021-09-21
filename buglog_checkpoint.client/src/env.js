75f54478b734263ecb1f79595aaf7e7ac64aa4d4
ShereneC <spyhopper@cableone.net>
2021-09-21 09:50:15 -0600

    deploying to heroku

--- buglog_checkpoint.client/src/env.js
+++ buglog_checkpoint.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'codeworksclassroom.auth0.com'
-export const audience = 'https://codeworksclassroom.com'
-export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
+export const domain = 'sc-codeworks.us.auth0.com'
+export const audience = 'https://BugLog.com'
+export const clientId = 'ps5xsLNhpLA8J8KbsLEaQC3epbEfyE5J'
